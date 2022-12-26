821c5b3b605c7d208e3131e8ac57354082f332d4
sunjae0902 <sunjaegg0902@naver.com>
2022-12-27 06:06:29 +0900

    Initial Commit

---
+++ 알고리즘_과제/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  알고리즘_과제
+//
+//  Created by SUNJAE on 2022/12/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
