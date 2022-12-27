91be44f0e253aa84631bab2349c5db1d413fb6f2
sunjae0902 <96765626+sunjae0902@users.noreply.github.com>
2022-12-27 23:01:06 +0900

    Update 과제1.c

--- 알고리즘_과제/과제1.c
+++ 알고리즘_과제/과제1.c
@@ -1,8 +1,18 @@
 //2022-2 알고리즘 과제 1
 /*
- 문제: Linker Emulation을 통한 n번째 피보나치 항을 구하는 c/c++ 프로그램을 개발한다.
+ 문제: 다음 상세형 재귀적 알고리즘을 참고하여 Linker Emulation을 통한 n번째 피보나치 항을 구하는 c/c++ 프로그램을 개발한다.
+int fin (int n) {
+ int t1, t2;
+ if (n<=1) return n;
+ else {           // return fib(n-1) + fib(n-2);
+  t1 = fib(n-1); // a 
+  t2 = fib(n-2); // b 
+  return t1+t2; // c
+  }
+}
+
  입력 예: input n: 10
- 출력 예: 55
+ 출력 예: 55 (result2에 저장된 10번째 피보나치 수)
  */
 
 
@@ -14,23 +24,23 @@ long count2=0;
 
 typedef struct fbARI{
     long long n,t1,t2,fv;
-    char nStmt; //현재 처리 문장
-    struct fbARI* prev; //특정 노드에서 다음 노드를 가리키는 포인터
+    char nStmt; //현재 처리 문장을 나타냄
+    struct fbARI* prev; //다음 노드를 가리키는 포인터
 }fbARI;
 
 typedef struct Stack{
     fbARI *top; //가장 최근에 생성한 AR을 가리키는 포인터
 }Stack;
 
-void initStack(Stack *stack){
+void initStack(Stack *stack){ //스택 초기화 함수
     stack->top=NULL;
 }
 
-int isEmpty(Stack *stack){
+int isEmpty(Stack *stack){ //스택이 비었는지 확인하는 함수, 비었다면 TRUE 리턴
     return stack->top==NULL;
 }
 
-void push(Stack *stack, long long data){
+void push(Stack *stack, long long data){ // 스택 push 함수
     count2++;
     fbARI *now=(fbARI *)malloc(sizeof(fbARI));
     now->n=data;
@@ -38,11 +48,11 @@ void push(Stack *stack, long long data){
     stack->top=now;
 }
 
-long long pop(Stack *stack){
+long long pop(Stack *stack){ // 스택 Pop함수
     fbARI *now;
     long long re;
 
-    if (isEmpty(stack)){
+    if (isEmpty(stack)){ // 스택이 비어있다면 pop하지 못함
         return 0;
     }
     now=stack->top;
@@ -59,7 +69,7 @@ int main(void){
     initStack(&stack);
     
     int n;
-    long long fv=0,result2=0;
+    long long fv=0,result2=0; 
   
     
     printf("input n: "); scanf("%d",&n);
@@ -72,7 +82,7 @@ int main(void){
 
     while(!isEmpty(&stack)){ //스택이 빌 때까지 반복
         fv=pop(&stack);
-        switch(cfbARI->nStmt){
+        switch(cfbARI->nStmt){ //현재 처리 문장의 상태에 따라 분기
             case 'a':
                 if (fv==1) {
                     result2+=fv;
@@ -128,4 +138,6 @@ int main(void){
                 
               }
     }
+ return result2;
    }
+
